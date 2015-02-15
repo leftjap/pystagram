469a58282fa2cb3fdcc77cb25f42396a3a09ba2b
leftjap <leftjap@gmail.com>
2015-02-15 15:06:40 +0900

    photos form에 댓글 폼 추

--- photos/forms.py
+++ photos/forms.py
@@ -7,3 +7,10 @@ class PhotoModelForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ('image_file', 'description',)
+
+
+class CommentModelForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
+        
