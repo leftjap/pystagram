7a269e527f5082710d887fc73b5f769f81508b63
leftjap <leftjap@gmail.com>
2015-02-17 16:38:17 +0900

    form에 태크 추가

--- photos/forms.py
+++ photos/forms.py
@@ -4,12 +4,14 @@ from django import forms
 from .models import Photo, Comment
 
 class PhotoModelForm(forms.ModelForm):
+    tags = forms.CharField(required=False, max_length=200)
     class Meta:
         model = Photo
-        fields = ('image_file', 'description',)
+        fields = ('image_file', 'tags', 'description',)
 
 
 class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
